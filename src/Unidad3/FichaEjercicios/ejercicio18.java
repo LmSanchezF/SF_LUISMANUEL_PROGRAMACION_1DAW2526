d108b9fa6f24223958dcb2d4507bf4486046f7d9
LuisManuelSF <LmSanchezf001@educarex.es>
2025-12-01 13:28:35 +0100

    1 diciembre

--- src/Unidad3/FichaEjercicios/ejercicio18.java
+++ src/Unidad3/FichaEjercicios/ejercicio18.java
@@ -3,39 +3,62 @@ import java.util.Scanner;
 public class ejercicio18 {
 
 	public static void main(String[] args) {
-	char S;
-	char R;
-	char M;
-	char D;
+	 
+	
 	Scanner teclado= new Scanner(System.in);
+	char opcion=teclado.nextLine().charAt(0);
 	System.out.println("Introduzca el primer numero");
 	int numero1=teclado.nextInt();
 	System.out.println("Introduzca el segundo numero");
 	int numero2=teclado.nextInt();
 	System.out.println(" Numero 1: " +numero1);
 	System.out.println(" Numero 2: " +numero2);
+	do {
 	System.out.println("------MENU------");
 	System.out.println(" S = suma ");
 	System.out.println(" R = resta ");
 	System.out.println(" M = multiplicacion ");
 	System.out.println(" D = division " );
-	System.out.println("Elija una opcion");
+	System.out.println(" F = finalizar");
 	
-	switch (char) {
 	
-	case S:
+	if (opcion=='S') {	
+	 System.out.println("El resultado de la suma es: " +(numero1+numero2));
+	 break;
+	 }else if (opcion=='R') {
+		 System.out.println("El resultado de la resta es: " +(numero1+numero2));
+		 break;
+	 }else if (opcion=='M') {
+		 System.out.println("El resultado de la multiplicacion es: " +(numero1*numero2));
+		 break;
+	 }else if (opcion=='D') {
+		 System.out.println("El resultado de la division es: " +(numero1/numero2));
+		 break;
+	 }else if (opcion=='F') {
+		 System.out.println("Finalizando....Hasta pronto!!! ");
+		 break;
 		
-	 System.out.println("El resultado de la suma es: " +);
 	}
 	
-	
-	
+	}while(opcion!=S) {
+		
 	}
+	}
+
+	 
+		
+	
+	
+	
+	
+	
+	
 	
 	
 		
-	}
+	
 	
 	}
 
-}
+
+
