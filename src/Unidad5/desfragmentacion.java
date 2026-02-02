4ba5a7734a5ba94c7234e7fcf195083fe5b28319
LuisManuelSF <LmSanchezf001@educarex.es>
2026-02-02 12:38:51 +0100

    actualizacion

---
+++ src/Unidad5/desfragmentacion.java
@@ -0,0 +1,26 @@
+package Unidad5;
+
+public class desfragmentacion {
+
+	static double numeros[]= {5.0, -1.0, 8.5, -1.0, 9.0, -1.0, 4.0};
+	
+	public static void main(String[] args) {
+	
+
+	}
+	
+	
+	public static void compactarArray() {
+	for (int i = 0; i < numeros.length;i++) {
+		if(numeros[i]==-1) {
+			System.out.println("numeros sin -1: "+(numeros[i]!=-1));
+		}else {
+		
+			
+		
+	}
+		
+	}	
+	}
+
+}
